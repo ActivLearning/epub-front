01b89cb2684f0f7fc7595fdaf023680e700c44dc
zhouyan <zhouyan622870@nd.com.cn>
2020-09-29 14:12:39 +0800

    适配移动端长按标注

--- static/epub/selection.js
+++ static/epub/selection.js
@@ -4,6 +4,11 @@ document.addEventListener('mouseup', function (e) {
     if (!sel.isCollapsed && true) selected(e); // TODO
     else window.parent.document.getElementById('select-menu').style.visibility = 'hidden';
 });
+document.addEventListener('touchend', function (e) {
+    var sel = window.getSelection();
+    if (!sel.isCollapsed) touchSelected(e); // TODO
+    else window.parent.document.getElementById('select-menu').style.visibility = 'hidden';
+});
 
 // 选中文本后的事件处理程序
 function selected(e) {
@@ -27,6 +32,32 @@ function selected(e) {
         menu.style.top = y - menuHeight + 'px';
     } else {
         menu.style.top = y + 'px'
+    }
+
+    menu.style.visibility = 'visible';
+}
+//移动端选中处理
+function touchSelected(e){
+    var x = e.changedTouches[0].clientX,
+        y = e.changedTouches[0].clientY,
+        menu = window.parent.document.getElementById('select-menu'),
+        menuHeight = window.parent.document.defaultView.getComputedStyle(menu, null).height,
+        menuWidth = window.parent.document.defaultView.getComputedStyle(menu, null).width,
+        screenHeight = window.innerHeight, //视口高度
+        screenWidth = window.parent.screen.availWidth;
+    // alert(window.screen.availHeight);
+    menuHeight = parseInt(menuHeight);
+    menuWidth = parseInt(menuWidth);
+
+    if ((screenWidth - x) > menuWidth) {
+        menu.style.left = x + 'px';   
+    } else {
+        menu.style.left = screenWidth - menuWidth + 'px';
+    }
+    if ((menuHeight + y) < screenHeight) {
+        menu.style.top = y + 40 + 'px';
+    } else {
+        menu.style.top = y - 40 - menuHeight + 'px'
     }
 
     menu.style.visibility = 'visible';
